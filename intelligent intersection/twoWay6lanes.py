f63ef08d71d0126790c5db707e78cafcd129bec7
Guoqq17 <44039514+Guoqq17@users.noreply.github.com>
2021-12-05 16:35:09 -0800

    modify

--- intelligent intersection/twoWay6lanes.py
+++ intelligent intersection/twoWay6lanes.py
@@ -18,11 +18,11 @@ from plexe import Plexe, ACC, CACC
 
 
 conflict_matrix = {
-    0:[], 
-    1:[4, 8, 10, 11], 
-    2:[4, 5, 7, 11], 
-    3:[], 
-    4:[1, 2, 7, 11], 
+    0:[],
+    1:[4, 8, 10, 11],
+    2:[4, 5, 7, 11],
+    3:[],
+    4:[1, 2, 7, 11],
     5:[2, 7, 8, 10],
     6:[],
     7:[2, 4, 5, 10],
@@ -36,13 +36,13 @@ VEHICLE_LENGTH = 4
 DISTANCE = 6  # inter-vehicle distance
 LANE_NUM = 12
 PLATOON_SIZE = 1
-SPEED = 16.6  # m/s
-V2I_RANGE = 200 
+SPEED = 10  # m/s
+V2I_RANGE = 200
 PLATOON_LENGTH = VEHICLE_LENGTH * PLATOON_SIZE + DISTANCE * (PLATOON_SIZE - 1)
-ADD_PLATOON_PRO = 0.3  
-ADD_PLATOON_STEP = 600
-MAX_ACCEL = 2.6
-#DECEL = SPEED**2/(2*(V2I_RANGE-25))  
+ADD_PLATOON_PRO = 1
+ADD_PLATOON_STEP = 2
+MAX_ACCEL = 3
+#DECEL = SPEED**2/(2*(V2I_RANGE-25))
 #DECEL = 3.5
 STOP_LINE = 15.0
 
@@ -52,7 +52,7 @@ def add_single_platoon(plexe, topology, step, lane):
     for i in range(PLATOON_SIZE):
         vid = "v.%d.%d.%d" %(step/ADD_PLATOON_STEP, lane, i)
         routeID = "route_%d" %lane   # route 0~11, one-to-one map with lane
-        traci.vehicle.add(vid, routeID, departPos=str(100-i*(VEHICLE_LENGTH+DISTANCE)), departSpeed=str(5), departLane=str(lane%3), typeID="vtypeauto")        
+        traci.vehicle.add(vid, routeID, departPos=str(100-i*(VEHICLE_LENGTH+DISTANCE)), departSpeed=str(5), departLane=str(lane%3), typeID="vtypeauto")
         plexe.set_path_cacc_parameters(vid, DISTANCE, 2, 1, 0.5)
         plexe.set_cc_desired_speed(vid, SPEED)
         plexe.set_acc_headway_time(vid, 1.5)
@@ -86,7 +86,7 @@ def compute_leaving_time(veh):
 def main():
     sumo_cmd = ['sumo-gui', '--duration-log.statistics', '--tripinfo-output', 'my_output_file.xml', '-c', 'cfg/twoWay6lanes.sumo.cfg']
     traci.start(sumo_cmd)
-    # 每次traci.simulationStep()之后都调用一次plexe 
+    # 每次traci.simulationStep()之后都调用一次plexe
     plexe = Plexe()
     traci.addStepListener(plexe)
 
@@ -95,53 +95,53 @@ def main():
     serving_list = []  # each element: [veh, route, leaving_time, priority]
     serving_list_veh_only = []  # each element: veh
 
-    while step < 360000:  # 1 hour       
-        
+    while step < 360000:  # 1 hour
+
         traci.simulationStep()
 
         if step % ADD_PLATOON_STEP == 0:  # add new platoon every X steps
-            add_platoons(plexe, topology, step) 
-        
+            add_platoons(plexe, topology, step)
+
 
         # check all leaders to decide whether add to serving list or delete from topology
         deleted_veh = []
-        for key, value in list(topology.items()):            
+        for key, value in list(topology.items()):
             if value == {}:  # if it is a leader
                 odometry = traci.vehicle.getDistance(key)
                 # for the first time V2I communication
-                if (not key in serving_list_veh_only) and (400-V2I_RANGE <= odometry < 400-V2I_RANGE+100): 
+                if (not key in serving_list_veh_only) and (400-V2I_RANGE <= odometry < 400-V2I_RANGE+100):
                     # add to serving list and initialize by simply setting leaving_time=0, priority=1.
                     serving_list.append([key, int(key.split(".")[2]), 0, 1])
                     serving_list_veh_only.append(key)
                 # record the platoon which has almost finished the route
                 if odometry > 800:
-                    deleted_veh.append(key)     
+                    deleted_veh.append(key)
 
         # delete the platoon which has almost finished the route
         for veh in deleted_veh:
             veh_time = veh.split(".")[1]
             veh_route = veh.split(".")[2]
-            for i in range(PLATOON_SIZE):                
+            for i in range(PLATOON_SIZE):
                 veh_id = "v." + veh_time + "." + veh_route + "." + str(i)
                 del topology[veh_id]
-            
+
 
         # delete vehcles from the list which has already passed the intersection
-        serving_list[:] = [element for element in serving_list if traci.vehicle.getDistance(element[0]) < 400 + PLATOON_LENGTH + STOP_LINE]  
-        serving_list_veh_only = [element[0] for element in serving_list]  
+        serving_list[:] = [element for element in serving_list if traci.vehicle.getDistance(element[0]) < 400 + PLATOON_LENGTH + STOP_LINE]
+        serving_list_veh_only = [element[0] for element in serving_list]
 
-        
+
         # update leaving_time for priority 0 and update speed for priority 1
         for i in range(len(serving_list)):  # serving_list element = [veh, route, leaving_time, priority]
             veh_i = serving_list[i][0]  # veh_ID = "v.time.route.num"
             route_i = int(veh_i.split(".")[2])
             priority = serving_list[i][3]
             if priority == 0: # for priority=0, only need to update leaving_time
-                serving_list[i][2] = compute_leaving_time(veh_i)                
+                serving_list[i][2] = compute_leaving_time(veh_i)
             else:
-                # find the max leaving time in conflict routes  
-                # initialize the time by a small value 
-                max_leaving_time = 0.00001            
+                # find the max leaving time in conflict routes
+                # initialize the time by a small value
+                max_leaving_time = 0.00001
                 for j in range(i):
                     veh_j = serving_list[j][0]
                     route_j = int(veh_j.split(".")[2])
@@ -181,16 +181,16 @@ def main():
                         #desired_speed = speed + DECEL*0.01
                         #if desired_speed > SPEED:
                         #    desired_speed = SPEED
-                        #traci.vehicle.slowDown(veh_i, desired_speed, 0.01) 
+                        #traci.vehicle.slowDown(veh_i, desired_speed, 0.01)
                         reset_veh(plexe, veh_i)
-                        #traci.vehicle.setSpeed(veh_i, desired_speed)                   
+                        #traci.vehicle.setSpeed(veh_i, desired_speed)
                     serving_list[i][2] = (distance_to_stop_line + PLATOON_LENGTH + 2*STOP_LINE)*1.0/speed
                     """
 
         if step % 10 == 1:
             # simulate vehicle communication every 0.1s
             communicate(plexe, topology)
-       
+
         step += 1
 
 
